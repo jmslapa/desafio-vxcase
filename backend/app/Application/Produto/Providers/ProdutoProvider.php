4eeaf2067957ac3d2a6be6e0eb5bcb0714c45599
= <=>
2021-01-29 00:44:58 -0300

    Ligações e injeções de dependências registradas no provider

--- backend/app/Application/Produto/Providers/ProdutoProvider.php
+++ backend/app/Application/Produto/Providers/ProdutoProvider.php
@@ -2,8 +2,19 @@
 
 namespace Application\Produto\Providers;
 
+use Application\Produto\Http\Controllers\ProdutoController;
+use Application\Produto\Http\ResourceFactories\ProdutoResourceFactory;
+use Application\Produto\Http\Resources\Produto as ProdutoResource;
+use Application\Produto\Http\Resources\ProdutoCollection;
+use Application\Produto\Http\Validators\ProdutoValidator;
+use Domain\Produto\ActionsContainers\ProdutoActionsContainer;
+use Domain\Produto\Models\Produto;
+use Domain\Produto\Repositories\ProdutoRepository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Support\Abstracts\Validator\BaseValidator;
 use Support\Contracts\ProviderContract;
+use Support\Contracts\ResourceFactoryContract;
 
 class ProdutoProvider extends ServiceProvider implements ProviderContract
 {
@@ -38,7 +49,7 @@ class ProdutoProvider extends ServiceProvider implements ProviderContract
      */
     public function registerRepository()
     {
-        //
+        app()->bind('produtoRepository', ProdutoRepository::class);
     }
 
     /**
@@ -48,7 +59,9 @@ class ProdutoProvider extends ServiceProvider implements ProviderContract
      */
     public function registerRepositoryDependencies()
     {
-        //
+        $this->app->when(ProdutoRepository::class)
+            ->needs(Model::class)
+            ->give(Produto::class);
     }
 
     /**
@@ -58,7 +71,8 @@ class ProdutoProvider extends ServiceProvider implements ProviderContract
      */
     public function registerResources()
     {
-       //
+       app()->bind('produto', ProdutoResource::class);
+       app()->bind('produtoCollection', ProdutoCollection::class);
     }
 
     /**
@@ -78,10 +92,17 @@ class ProdutoProvider extends ServiceProvider implements ProviderContract
      */
     public function registerControllerDependencies()
     {
-        // Actions Container        
+        // Actions Container   
+        app()->bind('produtoActionsContainer', ProdutoActionsContainer::class);     
         
         // Validator    
+        $this->app->when(ProdutoController::class)
+            ->needs(BaseValidator::class)
+            ->give(ProdutoValidator::class);
 
         // Resource Factory        
+        $this->app->when(ProdutoController::class)
+            ->needs(ResourceFactoryContract::class)
+            ->give(ProdutoResourceFactory::class);
     }
 }
