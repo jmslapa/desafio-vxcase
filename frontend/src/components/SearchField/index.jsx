ab0837d0278e857ef23c6cf89c5a437931c3c095
= <=>
2021-01-30 19:29:17 -0300

    Adicionada chamada ao dispatcher do reducer para guardar no estado 'search' o valor digitado no input de busca

--- frontend/src/components/SearchField/index.jsx
+++ frontend/src/components/SearchField/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Search } from 'styled-icons/fa-solid';
+import { changeSearchValue } from '../../store/modules/search/actions';
 import { Wrapper } from './styles';
 
 const SearchField = React.forwardRef(({ width, ...rest }, ref) => {
+
+    const search = useSelector(state => state.search);
+    const dispatch = useDispatch();
+
     return (
         <Wrapper width={width}>
-            <input {...rest} ref={ref} type="text" placeholder="Buscar..."/>
+            <input {...rest} ref={ref} type="text" placeholder="Buscar..."
+                value={search.value} onChange={e => dispatch(changeSearchValue(e.target.value))}
+            />
             <Search size={20}/>
         </Wrapper>
     )
