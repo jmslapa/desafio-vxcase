073ee326e6a183950c2ea954e4958138d200946c
= <=>
2021-01-30 01:28:13 -0300

    Implementados métodos make e makeCollection

--- backend/app/Application/Venda/Http/ResourceFactories/VendaResourceFactory.php
+++ backend/app/Application/Venda/Http/ResourceFactories/VendaResourceFactory.php
@@ -7,10 +7,10 @@ use Support\Contracts\ResourceFactoryContract;
 class VendaResourceFactory implements ResourceFactoryContract
 {
     public function make($resource) {
-        //
+        return app('venda', ['resource' => $resource]);
     }
 
     public function makeCollection($resource) {
-        //
+        return app('vendaCollection', ['resource' => $resource]);
     }
 }
