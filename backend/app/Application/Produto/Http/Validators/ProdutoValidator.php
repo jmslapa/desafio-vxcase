09dca3de26347f1e7fa8792832f92af0cde0399a
= <=>
2021-01-29 22:15:43 -0300

    Criado validation rules para a action store

--- backend/app/Application/Produto/Http/Validators/ProdutoValidator.php
+++ backend/app/Application/Produto/Http/Validators/ProdutoValidator.php
@@ -6,11 +6,28 @@ use Support\Abstracts\Validator\BaseValidator;
 
 class ProdutoValidator extends BaseValidator
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->customAttributes = [
+            'preco' => 'preço',
+            'entrega' => 'entrega (em dias)'
+        ];
+
+        $this->messages = [
+            'preco.regex' => 'O campo preço deve ser um valor decimal com a representação obrigatória '
+                            .'de 2 casas decimais separadas por vírgula e valor entre 0,00 e 99999999,99.',
+        ];
+    }
+
     protected function registerRules()
     {
         // Regras da action store
         $this->bind('store', [
-            //
+            'nome' => ['required', 'string', 'between:3,50'],
+            'preco' => ['required', 'regex:/^\d{1,8},\d{2}$/'],
+            'entrega' => ['required', 'integer', 'between:0,255'],
         ]);
 
         // Regras da action update
