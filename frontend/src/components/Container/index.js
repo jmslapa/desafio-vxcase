23b1861db5b747d15808519d68820fa62e0221f7
= <=>
2021-01-30 16:21:18 -0300

    Ajustes

--- frontend/src/components/Container/index.js
+++ frontend/src/components/Container/index.js
@@ -5,4 +5,5 @@ export default styled.div`
     height: 100%;
     width: 80%;
     margin: 0 auto;
+    padding: 40px 0;
 `;
--- frontend/src/components/TopBar/styles.js
+++ frontend/src/components/TopBar/styles.js
@@ -7,9 +7,6 @@ export const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    position: sticky;
-    top: 40px;
-    left: 0px;
     padding: 10px 15px;
     width: 100%;
     border-radius: 8px;
