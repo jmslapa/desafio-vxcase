5983611a369cbf34e78e3adf7745f3abc3e7bbf7
= <=>
2021-01-29 22:36:56 -0300

    Repositório agora implemente o SluggableRepositoryContrat e implementa o método findBySlug

--- backend/app/Domain/Produto/Repositories/ProdutoRepository.php
+++ backend/app/Domain/Produto/Repositories/ProdutoRepository.php
@@ -3,8 +3,12 @@
 namespace Domain\Produto\Repositories;
 
 use Support\Abstracts\Repository\BaseRepository;
+use Support\Contracts\SluggableRepositoryContract;
 
-class ProdutoRepository extends BaseRepository
+class ProdutoRepository extends BaseRepository implements SluggableRepositoryContract
 {
-    //
+    public function findBySlug(string $slug, $fail = true)
+    {
+        return $this->model->findBySlug($slug, $fail);
+    }
 }
