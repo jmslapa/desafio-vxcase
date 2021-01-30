2f669d2d7dd8e10b3762b41cc174d14dfe5460b3
= <=>
2021-01-29 22:15:18 -0300

    Modificado o método método findBySslug, adicionado método abstrato getSluggableAttribute para que o slug seja baseado em uma tributo definido dinamicamente e cirrugida a substituição de caracteres especiais do valor utilizado como base para o slug

--- backend/app/Support/Traits/Sluggable.php
+++ backend/app/Support/Traits/Sluggable.php
@@ -3,8 +3,29 @@
 namespace Support\Traits;
 
 use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 
 trait Sluggable {
+
+    /**
+     * Retorna uma string contendo o nome do atributo a ser utilizado como base para a geração do
+     *
+     * @return string
+     */
+    abstract protected static function getSluggableAttribute() :string;    
+
+    /**
+     * Realiza busca por um registro através do slug
+     *
+     * @param string $slug
+     * @param boolean $fail
+     * @return Model
+     */
+    protected function findBySlug(string $slug, $fail = true)
+    {
+        $query = $this->query()->where('slug', $slug);
+        return $fail ? $query->firstOrFail() : $query->first();
+    }
 
     /**
      * The "booted" method of the model.
@@ -15,13 +36,12 @@ trait Sluggable {
     {
         parent::booted();
         static::creating(function($produto) {
-            $nome = iconv('UTF-8','ASCII//TRANSLIT', $produto->nome);
-            $produto->nome = Str::slug($nome);
+            setlocale(LC_ALL, "en_US.utf8");
+            $asciiAttribute = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $produto->getAttribute(static::getSluggableAttribute()));
+            $produto->slug = Str::slug($asciiAttribute);
+            if($produto->findBySlug($produto->slug, false)) {
+                throw new UnprocessableEntityHttpException('Já existe um produto com nome semelhante, realize alguma modificação.');
+            }
         });
-    }
-
-    protected function findBySlug($slug)
-    {
-        return $this->where('slug', $slug)->firstOrFail();
     }
 }
