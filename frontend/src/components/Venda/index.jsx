f473d7f86176a6efa038cc2b1b3994398c7bb609
= <=>
2021-01-31 01:30:33 -0300

    Correção nos valores de entrega prevista e adicionado 'R$' ao total

--- frontend/src/components/Venda/index.jsx
+++ frontend/src/components/Venda/index.jsx
@@ -6,7 +6,7 @@ import moment from 'moment-business-days';
 
 const Venda = React.forwardRef(({ venda }, ref) => {
 
-    const entregaPrevsta = () => {
+    const entregaPrevista = () => {
         return moment(venda.momento, 'DD/MM/YYYY').businessAdd(venda.entrega_prevista).format('DD/MM/YYYY');
     }
 
@@ -15,8 +15,8 @@ const Venda = React.forwardRef(({ venda }, ref) => {
             <Row width="100%" justify="space-between" className="data">
                 <div className="data-item">Venda: {venda.id}</div>
                 <div className="data-item">Momento: {venda.momento}</div>
-                <div className="data-item">Previsão de entrega: </div>
-                <div className="data-item">Total: {venda.subtotal}</div>
+                <div className="data-item">Entrega Prevista: {entregaPrevista()}</div>
+                <div className="data-item">Total: R$ {venda.subtotal}</div>
             </Row>
 
             <Row width="100%" direction="column" className="items">
